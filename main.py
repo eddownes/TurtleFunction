22c3bb77e0c87a80adc972fbd48a1fc9d6d3179a
Ed Downes <eddownes@outlook.com>
2020-01-10 18:41:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+#square draw
+def my_square():
+  my_turtle.forward(90)
+  my_turtle.left(90)
+  my_turtle.forward(90)
+  my_turtle.left(90)
+  my_turtle.forward(90)
+  my_turtle.left(90)
+  my_turtle.forward(90)
+  my_turtle.left(90)
+
+my_square()
